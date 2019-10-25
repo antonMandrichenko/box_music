330dafe848fd02c09304c35ce712ee6cc9d0aea0
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-10-25 17:23:20 +0300

    Formatted code (i1)

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import MainTabNavigator from "./MainTabNavigator";
 import SplashLoading from "../screens/SplashLoading";
 
 export default createAppContainer(
   createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    // Main: MainTabNavigator,
     Load: SplashLoading
   })
 );
--- navigation/AppNavigator.web.js
+++ navigation/AppNavigator.web.js
@@ -1,13 +1,11 @@
-import { createBrowserApp } from '@react-navigation/web';
-import { createSwitchNavigator } from 'react-navigation';
+import { createBrowserApp } from "@react-navigation/web";
+import { createSwitchNavigator } from "react-navigation";
 
 import SplashLoading from "../screens/SplashLoading";
 
 const switchNavigator = createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Load: SplashLoading
 });
-switchNavigator.path = '';
+switchNavigator.path = "";
 
-export default createBrowserApp(switchNavigator, { history: 'hash' });
+export default createBrowserApp(switchNavigator, { history: "hash" });
