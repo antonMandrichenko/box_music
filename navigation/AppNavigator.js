c2a6f2b595d1013373607e416d969365c885a7a0
Anton <arnellomutty2@gmail.com>
2019-10-29 22:13:50 +0200

    Implemented second page

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,11 @@ import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
 import SplashLoading from "../screens/SplashLoading";
+import androidLogin from "../screens/androidLogin"
 
 export default createAppContainer(
   createSwitchNavigator({
-    Load: SplashLoading
+    Load: SplashLoading,
+    Login: androidLogin
   })
 );
--- navigation/AppNavigator.web.js
+++ navigation/AppNavigator.web.js
@@ -2,9 +2,11 @@ import { createBrowserApp } from "@react-navigation/web";
 import { createSwitchNavigator } from "react-navigation";
 
 import SplashLoading from "../screens/SplashLoading";
+import Login from "../screens/Login"
 
 const switchNavigator = createSwitchNavigator({
-  Load: SplashLoading
+  Load: SplashLoading,
+  Login: Login
 });
 switchNavigator.path = "";
 
