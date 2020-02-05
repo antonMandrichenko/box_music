c01da3b9efb7354e1d96b35e5bb36b0b197ca4ec
Anton <arnellomutty2@gmail.com>
2020-02-05 10:45:25 +0200

    Fixed timer android

--- context/AppContext.js
+++ context/AppContext.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, memo } from "react";
+import React, { useState, useEffect } from "react";
 import firebase from "../config/firebase";
 
 const AppContext = React.createContext();
-import { AsyncStorage } from "react-native";
+import { AsyncStorage, Platform, InteractionManager } from "react-native";
 import { radioPlaylist } from "../api/RadioPlaylist";
 
 const AppProvider = ({ children }, props) => {
@@ -235,6 +235,47 @@ const AppProvider = ({ children }, props) => {
     setSwitchPlan(!switchPlan);
   };
 
+    const _setTimeout = global.setTimeout;
+    const _clearTimeout = global.clearTimeout;
+    const MAX_TIMER_DURATION_MS = 60 * 1000;
+    if (Platform.OS === 'android') {
+        const timerFix = {};
+        const runTask = (id, fn, ttl, args) => {
+            const waitingTime = ttl - Date.now();
+            if (waitingTime <= 1) {
+                InteractionManager.runAfterInteractions(() => {
+                    if (!timerFix[id]) {
+                        return;
+                    }
+                    delete timerFix[id];
+                    fn(...args);
+                });
+                return;
+            }
+
+            const afterTime = Math.min(waitingTime, MAX_TIMER_DURATION_MS);
+            timerFix[id] = _setTimeout(() => runTask(id, fn, ttl, args), afterTime);
+        };
+
+        global.setTimeout = (fn, time, ...args) => {
+            if (MAX_TIMER_DURATION_MS < time) {
+                const ttl = Date.now() + time;
+                const id = '_lt_' + Object.keys(timerFix).length;
+                runTask(id, fn, ttl, args);
+                return id;
+            }
+            return _setTimeout(fn, time, ...args);
+        };
+
+        global.clearTimeout = id => {
+            if (typeof id === 'string' && id.startWith('_lt_')) {
+                _clearTimeout(timerFix[id]);
+                delete timerFix[id];
+                return;
+            }
+            _clearTimeout(id);
+        };
+    }
   return (
     <AppContext.Provider
       value={{
